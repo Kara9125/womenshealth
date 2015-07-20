d0ffbd52c737d2bb676b802f33c6850f7cab2d6e
Kara9125 <K.nicholas9125@gmail.com>
2015-07-20 13:48:37 -0700

    ready for first deploy

--- server.js
+++ server.js
@@ -4,5 +4,11 @@ var app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 });
+
+mongoose.connect(
+  process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/womenshealth' // plug in the db name you've been using
+);
  
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
