61d39309a91a92bc63240f6931838ceecb8f361b
Kara9125 <K.nicholas9125@gmail.com>
2015-07-20 14:13:02 -0700

    ready for frist deploy

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser'),
+var mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://localhost/test');
  
 app.get('/', function (req, res) {
   res.send('Hello World')
